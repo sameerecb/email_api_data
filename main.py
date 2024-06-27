321b9b4cfc718788424d9c63d5f883610a1bc2b6
Sameer Bansal <sameerecb@gmail.com>
2024-06-27 20:04:20 +0530

    Update main.py
    Removed API keys

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 
-api_key = "65f400e36d21456eb4e2ca7bba8328a1"
+api_key = "Enter API KEY"
 url = "https://newsapi.org/v2/everything?q=tesla&" \
       "from=2024-05-27&sortBy=publishedAt&" \
-      "apiKey=65f400e36d21456eb4e2ca7bba8328a1"
+      "apiKey=Enter API Key"
 # Make request
 request = requests.get(url)
 # Get dictionary with data
