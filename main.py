8923e9b2004eb747a371e9ba24ed933f9b36e531
sameer <sameerecb@gmail.com>
2024-07-05 15:35:22 +0530

    Send API data over email

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import requests
-
-api_key = "65f400e36d21456eb4e2ca7bba8328a1"
+from send_email import send_email
+api_key = "API_KEY"
 url = "https://newsapi.org/v2/everything?q=tesla&" \
-      "from=2024-05-27&sortBy=publishedAt&" \
-      "apiKey=65f400e36d21456eb4e2ca7bba8328a1"
+      "from=2024-06-05&sortBy=publishedAt&apiKey=" \
+      "<API_KEY"
 # Make request
 request = requests.get(url)
 # Get dictionary with data
 content = request.json()
 # Access the article tile and description
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    # print(article["title"])  # To print on console
+    # print(article["description"])  # To print on console
+    if article["title"] is not None:
+        if article["description"] is not None:
+            body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+# print(body)
+body = body.encode("utf-8")
+send_email(message=body)
+
---
+++ send_email.py
@@ -0,0 +1,17 @@
+# link to create app passwords https://myaccount.google.com/apppasswords"
+import smtplib
+import ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "theflexfound@gmail.com"
+    password = "Email_Password"
+
+    receiver = "theflexfound@gmail.com"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
